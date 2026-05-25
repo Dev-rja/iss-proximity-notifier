e487d9be6fad42c431567399a24d2352717efa67
Dev-rja <130688309+Dev-rja@users.noreply.github.com>
2026-05-25 19:17:15 +0800

    feat: use city name from config throughout frontend via Jinja

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ MY_LAT  = 8.48
 MY_LON  = 124.65
 
 # Alert when ISS is within this many kilometers
-ALERT_RADIUS_KM = 6000
+ALERT_RADIUS_KM = 500
 
 # Proximity panel display radius (wider, for awareness)
-PROXIMITY_RADIUS = 6000
+PROXIMITY_RADIUS = 1000
 
 # How often to check (in seconds)
 POLL_INTERVAL = 60
